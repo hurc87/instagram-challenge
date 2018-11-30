adea5296522308095f7066036a52a11f5f809a18
hurc87 <zoe1987@btinternet.com>
2018-11-30 11:33:24 +0000

    added token for signing in

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://qwerty:qwerty123@ds143774.mlab.com:43774/instagram_clone'
+  mongoURI: 'mongodb://qwerty:qwerty123@ds143774.mlab.com:43774/instagram_clone',
+  secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load user model
 const User = require('../../models/User');
@@ -57,7 +59,22 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if(isMatch) {
-            res.json({msg: 'Success'});
+            // User matched
+
+            // create JWT payload
+            const payload = { id: user.id, name: user.name}
+
+            // Sign jsonwebtoken
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success:true,
+                  token: 'Bearer '+ token
+                });
+              });
           } else {
             return res.status(400).json({password: 'Password incorrect'});
           }
