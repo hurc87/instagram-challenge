f90b7f287e5f02b6e126ce298e16644d2b387a24
hurc87 <zoe1987@btinternet.com>
2018-11-30 12:00:11 +0000

    added private route and full authorisation

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load user model
 const User = require('../../models/User');
@@ -64,7 +65,7 @@ router.post('/login', (req, res) => {
             // create JWT payload
             const payload = { id: user.id, name: user.name}
 
-            // Sign jsonwebtoken
+            // Sign Token
             jwt.sign(
               payload,
               keys.secretOrKey,
@@ -80,6 +81,19 @@ router.post('/login', (req, res) => {
           }
         });
     });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current',
+passport.authenticate('jwt', { session: false }),
+(req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
