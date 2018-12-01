58eb1e7de0d9dab9a5318e097cdbf760eff417d6
hurc87 <zoe1987@btinternet.com>
2018-12-01 19:34:10 +0000

    added login and register routes

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Registration page</h1>
+      </div>
+    );
+  }
+}
+
+export default Register;
